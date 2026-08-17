0a6a1227ab682244e701cbd16631b38e9b4644a4
samyakweb <samyakwork23@gmail.com>
2026-08-17 22:02:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// Smooth scrolling for website links
+
+document.querySelectorAll('a[href^="#"]').forEach(function(link) {
+
+    link.addEventListener("click", function(event) {
+
+        const target = document.querySelector(
+            this.getAttribute("href")
+        );
+
+        if (target) {
+            event.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+});
